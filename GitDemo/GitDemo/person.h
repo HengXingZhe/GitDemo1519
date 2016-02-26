4ae646013252399dd047704aa1f5a11b8591d447
tshb <906472764@qq.com>
2016-02-26 14:17:09 +0800

    work1 创建 person类

---
+++ GitDemo/GitDemo/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 田守彬. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ GitDemo/GitDemo/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  GitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 田守彬. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
